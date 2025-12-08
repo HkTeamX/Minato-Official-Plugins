ba4a5e5e308ecd450ef0d6029389fb78be5ab185
huankong233 <huank0ng@foxmail.com>
2025-12-08 16:01:21 +0800

    [-] 移除无用包 [*] 修改包简介 [+] 增加请假包

---
+++ packages/minato-plugin-wxjsxy/src/api.ts
@@ -0,0 +1,120 @@
+import CryptoJS from 'crypto-js'
+import { type AxiosConfig, Axios } from '@atri-bot/lib-request'
+// @ts-expect-error 函数来自老库
+import { RSAKeyPair, setMaxDigits, encryptedString } from './lib.js'
+
+setMaxDigits(131)
+
+export class Api {
+  axios: Axios
+
+  constructor(config: AxiosConfig) {
+    this.axios = new Axios(config)
+  }
+
+  exponentHex = '010001'
+  modulusHex =
+    '00b5eeb166e069920e80bebd1fea4829d3d1f3216f2aabe79b6c47a3c18dcee5fd22c2e7ac519cab59198ece036dcf289ea8201e2a0b9ded307f8fb704136eaeb670286f5ad44e691005ba9ea5af04ada5367cd724b5a26fdb5120cc95b6431604bd219c6b7d83a6f8f24b43918ea988a76f93c333aa5a20991493d4eb1117e7b1'
+  key = new RSAKeyPair(this.exponentHex, '', this.modulusHex)
+
+  loginToCas(
+    username: string,
+    password: string,
+  ): Promise<{ data: { tgt: string; ticket: string } }> {
+    return this.axios.post({
+      url: 'https://cas.wxjsxy.com.cn/lyuapServer/v1/tickets',
+      data: {
+        username,
+        password: encryptedString(this.key, password),
+        service: 'https://portal.wxjsxy.com.cn/',
+        loginType: '',
+        id: '',
+        code: '',
+        otpcode: '',
+      },
+    })
+  }
+
+  getCasLoginToken(ticket: string): Promise<{ data: string }> {
+    return this.axios.post({
+      url: `https://cas.wxjsxy.com.cn/lyuapServer/v1/tickets/${ticket}`,
+      data: {
+        service: 'http://dy.wxjsxy.com.cn/prdapi/wxjsxyapp/cas/index',
+        loginToken: 'loginToken',
+      },
+    })
+  }
+
+  async getDyCookie(ticket: string): Promise<string> {
+    const result = await this.axios.get({
+      url: `http://dy.wxjsxy.com.cn/prdapi/wxjsxyapp/cas/index?ticket=${ticket}`,
+    })
+    const setCookie = result.headers['set-cookie']
+    if (!setCookie) throw new Error('Failed to get CAS cookie')
+    return setCookie.map((cookie) => cookie.split(';')[0] + ';').join('')
+  }
+
+  getDyToken(cookie: string): Promise<{ data: { token: string } }> {
+    return this.axios.get({
+      url: 'http://dy.wxjsxy.com.cn/prdapi/wxjsxyapp/cas/indexData',
+      headers: {
+        cookie,
+      },
+    })
+  }
+
+  setDyProcess(
+    token: string,
+    beginTime: string,
+    endTime: string,
+    leaveSchool: '是' | '否',
+    backDormitory: '是' | '否',
+    askedType: '事假' | '病假' | '节假日',
+    reason: string,
+  ) {
+    const data = {
+      beginTime,
+      endTime,
+      leaveSchool,
+      backDormitory,
+      askedType,
+      reason,
+      processDefinitionKey: 'studentApply',
+    }
+    const sign = CryptoJS.MD5(`myappsecret${JSON.stringify(data)}myappsecret`).toString()
+    return this.axios.post({
+      url: `http://dy.wxjsxy.com.cn/prdapi/activiti/processInstance/startProcess?user_info_query_json=${encodeURIComponent(JSON.stringify(data))}`,
+      data,
+      headers: {
+        token,
+        mode: 'wxa',
+        sign,
+        timestamp: new Date().getTime(),
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
+  getDyProcessList(
+    token: string,
+  ): Promise<{ data: { rows: { approvaState: string; processStartTime: string }[] } }> {
+    const data = {
+      pageNo: 1,
+      pageSize: 5,
+    }
+    const sign = CryptoJS.MD5(`myappsecret${JSON.stringify(data)}myappsecret`).toString()
+    return this.axios.post({
+      url: `http://dy.wxjsxy.com.cn/prdapi/activiti/task/myapply?user_info_query_json=${encodeURIComponent(JSON.stringify(data))}`,
+      data,
+      headers: {
+        token,
+        mode: 'wxa',
+        sign,
+        timestamp: new Date().getTime(),
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+}
---
+++ packages/minato-plugin-wxjsxy/src/index.ts
@@ -0,0 +1,193 @@
+import { Api } from './api'
+import { BasePlugin, type CommandCallback } from '@atri-bot/core'
+import { Command } from 'commander'
+import dayjs from 'dayjs'
+import { Structs } from 'node-napcat-ts'
+
+export interface WxjsxyAccount {
+  username: string
+  password: string
+}
+
+export interface WxjsxyPluginConfig {
+  accounts: Record<number, WxjsxyAccount>
+}
+
+export interface WxjsxyCommandContext {
+  params: {
+    action: '添加账号' | '请假' | '签到情况'
+  }
+}
+
+export interface WxjsxyAddAccountCommandContext {
+  params: {
+    username: string
+    password: string
+  }
+}
+
+export class Plugin extends BasePlugin<WxjsxyPluginConfig> {
+  configName = '@minato-bot__atri-bot-plugin-wxjsxy'
+  defaultConfig: WxjsxyPluginConfig = {
+    accounts: [],
+  }
+
+  api = new Api({
+    ...this.atri.config,
+    createAxios: {
+      withCredentials: true,
+      maxRedirects: 0, // 某些网站跳转后不再返回 set-cookie，可以视情况禁用自动重定向
+      validateStatus: (s) => s === 302 || s === 200, // 允许 302
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'sec-ch-ua-platform': '"Windows"',
+      },
+    },
+  })
+
+  async load() {
+    this.regCommandEvent({
+      commandName: 'wxjsxy添加账号',
+      commander: new Command()
+        .description('钉钉请假添加账号')
+        .requiredOption('-u, --username <username>', '账号')
+        .requiredOption('-p, --password <password>', '密码'),
+      callback: this.addAccount.bind(this),
+    })
+
+    this.regCommandEvent({
+      commandName: 'wxjsxy删除账号',
+      commander: new Command().description('钉钉请假删除账号'),
+      callback: this.removeAccount.bind(this),
+    })
+
+    this.regCommandEvent({
+      commandName: 'wxjsxy请假',
+      commander: new Command().description('钉钉请假'),
+      callback: this.process.bind(this),
+    })
+
+    this.regCommandEvent({
+      commandName: 'wxjsxy请假情况',
+      commander: new Command().description('钉钉请假情况'),
+      callback: this.processList.bind(this),
+    })
+  }
+
+  unload() {}
+
+  async addAccount({ context, params }: CommandCallback<WxjsxyAddAccountCommandContext>) {
+    const userId = context.user_id
+
+    if (this.config.accounts[userId]) {
+      await this.bot.sendMsg(context, [Structs.text('账号已存在，无需重复添加')])
+      return
+    }
+
+    // 尝试登录
+    try {
+      const loginRes = await this.api.loginToCas(params.username, params.password)
+
+      await this.bot.sendMsg(context, [
+        Structs.text(`登录成功, 返回信息: \n ${JSON.stringify(loginRes.data)}`),
+      ])
+
+      this.config.accounts[userId] = {
+        username: params.username,
+        password: params.password,
+      }
+
+      this.saveConfig()
+    } catch (error) {
+      await this.bot.sendMsg(context, [
+        Structs.text(`登录失败，请检查账号密码是否正确, 错误日志: ${error}`),
+      ])
+
+      return
+    }
+  }
+
+  async removeAccount({ context }: CommandCallback) {
+    const userId = context.user_id
+
+    if (!this.config.accounts[userId]) {
+      await this.bot.sendMsg(context, [Structs.text('账号不存在，无法删除')])
+      return
+    }
+
+    delete this.config.accounts[userId]
+    await this.bot.sendMsg(context, [Structs.text('账号删除成功')])
+  }
+
+  async process({ context }: CommandCallback) {
+    const userId = context.user_id
+    const account = this.config.accounts[userId]
+
+    if (!account) {
+      await this.bot.sendMsg(context, [
+        Structs.text('请先添加账号，使用命令：wxjsxy添加账号 -u <用户名> -p <密码>'),
+      ])
+      return
+    }
+
+    // 尝试登录
+    try {
+      const loginRes = await this.api.loginToCas(account.username, account.password)
+      const loginTokenRes = await this.api.getCasLoginToken(loginRes.data.tgt)
+      const casCookie = await this.api.getDyCookie(loginTokenRes.data)
+      const dyTokenRes = await this.api.getDyToken(casCookie)
+      const dyAction = await this.api.setDyProcess(
+        dyTokenRes.data.token,
+        dayjs().hour(6).minute(30).add(1, 'day').format('YYYY-MM-DD HH:mm'),
+        dayjs().hour(20).minute(50).add(1, 'day').format('YYYY-MM-DD HH:mm'),
+        '是',
+        '是',
+        '事假',
+        '集训',
+      )
+      await this.bot.sendMsg(context, [
+        Structs.text(`请假成功, 返回信息: \n ${JSON.stringify(dyAction.data)}`),
+      ])
+    } catch (error) {
+      await this.bot.sendMsg(context, [Structs.text(`操作失败，错误日志: ${error}`)])
+      return
+    }
+  }
+
+  async processList({ context }: CommandCallback) {
+    const userId = context.user_id
+    const account = this.config.accounts[userId]
+
+    if (!account) {
+      await this.bot.sendMsg(context, [
+        Structs.text('请先添加账号，使用命令：wxjsxy添加账号 -u <用户名> -p <密码>'),
+      ])
+      return
+    }
+
+    // 尝试登录
+    try {
+      const loginRes = await this.api.loginToCas(account.username, account.password)
+      const loginTokenRes = await this.api.getCasLoginToken(loginRes.data.tgt)
+      const casCookie = await this.api.getDyCookie(loginTokenRes.data)
+      const dyTokenRes = await this.api.getDyToken(casCookie)
+      const dyProcessList = await this.api.getDyProcessList(dyTokenRes.data.token)
+      await this.bot.sendMsg(context, [
+        Structs.text(`请假情况:\n`),
+        Structs.text(
+          dyProcessList.data.rows
+            .map(
+              (item) => `- 申请时间: ${item.processStartTime}\n   审核状态: ${item.approvaState}\n`,
+            )
+            .join(''),
+        ),
+      ])
+    } catch (error) {
+      await this.bot.sendMsg(context, [Structs.text(`操作失败，错误日志: ${error}`)])
+      return
+    }
+  }
+}
